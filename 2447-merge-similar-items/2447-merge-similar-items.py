196fcb476b11afefde47cbcd1cfcffdb5b1f94bf
Yonatan Gizaw <achiyodas@gmail.com>
2023-09-29 19:55:16 +0300

    Time: 117 ms (80.78%), Space: 17 MB (84.46%) - LeetHub

--- 2447-merge-similar-items/2447-merge-similar-items.py
+++ 2447-merge-similar-items/2447-merge-similar-items.py
@@ -1,13 +1,7 @@
+
+from collections import Counter
 class Solution:
     def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
-        res = items1
-        for i in range(len(items1)):
-            for j in range(len(items2)):
-                if items1[i][0] == items2[j][0]:
-                    res[i][1] = items1[i][1] + items2[j][1]
-        arr = [i[0] for i in items1]
-        for j in items2:
-            if j[0] not in arr:
-                res.append(j)
-        res.sort()
-        return res
+        cnt = Counter(dict(items1))
+        cnt += Counter(dict(items2))
+        return sorted(cnt.items())
