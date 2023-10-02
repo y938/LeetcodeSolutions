72db5a118c7944179ea8cab4b64fecd24a100886
Yonatan Gizaw <achiyodas@gmail.com>
2023-10-02 19:26:49 +0300

    Time: 62 ms (41.23%), Space: 16.5 MB (41.9%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -0,0 +1,16 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        lr , ud = 0 , 0
+
+        for move in moves:
+            if move == 'U':
+                ud += 1
+            elif move == 'D':
+                ud -= 1
+            elif move == 'L':
+                lr += 1
+            elif move == 'R':
+                lr -= 1
+
+        if lr == 0 and ud == 0:
+            return True
